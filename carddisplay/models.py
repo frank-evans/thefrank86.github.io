611a49239dd5b19287b038d745d291c5915b12dd
Frank <thefrank86@gmail.com>
2022-08-31 03:10:10 -0500

    cleaned up comments

--- carddisplay/models.py
+++ carddisplay/models.py
@@ -2,6 +2,7 @@ from django.db import models
 # import pandas as pd
 
 # Create your models here.
+
 #class Question(models.Model):
     #question_text = models.CharField(max_length=200)
     #pub_date = models.DateTimeField('date published')
@@ -12,18 +13,6 @@ from django.db import models
     #votes = models.IntegerField(default=0)
 
 
-# df1 = pd.read_csv('https://api.scryfall.com/cards/search?format=csv&q=s:neo ', usecols = ['set', 'name', 'image_uri'])
-
-# <html>
-# x=0
-# for x in range of len(df1-1)
-# <div>
-#    <img src="{{df1.iloc[x, 1]}}" />
-# </div>
-# </html>
-
-# .iloc[row, column]
-# print(df1.iloc[row, 2])
 class card(models.Model):
     set = models.CharField(max_length=64)
     name = models.CharField(max_length=64)
--- carddisplay/views.py
+++ carddisplay/views.py
@@ -3,20 +3,13 @@ from django.shortcuts import render
 from .models import *
 from django.core.management import call_command
 
-#call_command('flush')
+# reset object data for each search
 card.objects.all().delete()
+# update object data based on search keyword
 call_command('updatemodels')
+
 # Create your views here.
 def index(request):
-    #df1 = pd.read_csv('https://api.scryfall.com/cards/search?format=csv&q=s:neo ', usecols = ['set', 'name', 'image_uri'])
-
-# is any of this going to work?
-    #df1.set_index("name", inplace=True)
-    #card.image = df1.loc[['{name}'], ['image_uri']]
-    #card.name = {name}
-    #card.set = df1.loc[['{name}'], ['set']] 
-
-    #call_command('flush')
 
     return render(request, "carddisplay/index.html", {
         "carddisplay": card.objects.all()
