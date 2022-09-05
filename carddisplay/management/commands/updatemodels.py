d3042ff8155bbf915057704220a1cdf704ce0116
Frank <thefrank86@gmail.com>
2022-09-05 18:00:53 -0500

    updated html select formating css

--- carddisplay/management/commands/updatemodels.py
+++ carddisplay/management/commands/updatemodels.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 import pandas as pd
 from carddisplay.models import card
 # , search
-# from views import *
+
 
 class Command(BaseCommand):
     help = 'import booms'
@@ -14,6 +14,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         sellect = kwargs['sellect']
         #unique = "s:dmu"
+        #unique = "cardname-cardname"
         unique = f"s:{sellect}"
         df1 = pd.read_csv(f"https://api.scryfall.com/cards/search?format=csv&q={unique} ", usecols = ['set', 'name', 'image_uri'])
         for SET,NAME,IMAGE in zip(df1.set,df1.name,df1.image_uri):
