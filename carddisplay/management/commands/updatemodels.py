7746ee37f62d31f0cb22892a263f5e9c25740322
Frank <thefrank86@gmail.com>
2022-09-05 17:13:08 -0500

    slow but functioning SET search

--- carddisplay/management/commands/updatemodels.py
+++ carddisplay/management/commands/updatemodels.py
@@ -1,18 +1,21 @@
 from django.core.management.base import BaseCommand
 import pandas as pd
-from carddisplay.models import card, search 
-
+from carddisplay.models import card
+# , search
+# from views import *
 
 class Command(BaseCommand):
     help = 'import booms'
 
     def add_arguments(self, parser):
-        pass
+        #pass
+        parser.add_argument('-s', '--sellect', type=str)
 
-    def handle(self, *args, **options):
-
-        search = "s:jmp"
-        df1 = pd.read_csv(f"https://api.scryfall.com/cards/search?format=csv&q={search} ", usecols = ['set', 'name', 'image_uri'])
+    def handle(self, *args, **kwargs):
+        sellect = kwargs['sellect']
+        #unique = "s:dmu"
+        unique = f"s:{sellect}"
+        df1 = pd.read_csv(f"https://api.scryfall.com/cards/search?format=csv&q={unique} ", usecols = ['set', 'name', 'image_uri'])
         for SET,NAME,IMAGE in zip(df1.set,df1.name,df1.image_uri):
             models=card(set=SET,name=NAME,image=IMAGE)
             models.save()
--- carddisplay/views.py
+++ carddisplay/views.py
@@ -1,23 +1,28 @@
 from unicodedata import name
+from django.http import HttpResponseRedirect 
 from django.shortcuts import render
 from .models import *
 from django.core.management import call_command
 
-# reset object data for each search
-card.objects.all().delete()
-# update object data based on search keyword
-call_command('updatemodels')
 
 # Create your views here.
 def index(request):
 
-    return render(request, "carddisplay/index.html", {
-        "carddisplay": card.objects.all()
-    })
+        return render(request, "carddisplay/index.html", {
+            "carddisplay": card.objects.all()
+        })
 
 
 def result(request):
-    #search = search.objects.get(pk=type_search)
-    return render(request, "carddisplay/result.html", {
-        "carddisplay": card.objects.all()
-    })
+    if request.method == "POST":
+
+        # reset object data for each search
+        card.objects.all().delete()
+
+        # update object data based on set sellected
+        setsellect = request.POST["setsellect"]
+        call_command('updatemodels', sellect=setsellect)
+
+        return render(request, "carddisplay/result.html", {
+            "carddisplay": card.objects.all()
+        })
