9017a53cf2ced2e7fb96d7ff3af4a20984650c77
Frank <thefrank86@gmail.com>
2022-09-22 13:42:17 -0500

    added 3D hover effect to card images

---
+++ static/tilt.js
@@ -0,0 +1,13 @@
+const card = document.querySelector(".card");
+const THRESHOLD = 15;
+
+function handleHover(e) {
+	// TODO
+}
+
+function resetStyles(e) {
+	// TODO
+}
+
+card.addEventListener("mousemove", handleHover);
+card.addEventListener("mouseleave", resetStyles);
