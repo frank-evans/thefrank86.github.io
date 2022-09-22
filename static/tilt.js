3ddfe2a955da7ba46d007fb7ec0eaeaef9751494
Frank <thefrank86@gmail.com>
2022-09-22 17:45:49 -0500

    animation corrections

--- static/tilt.js
+++ static/tilt.js
@@ -1,13 +1,33 @@
-const card = document.querySelector(".card");
-const THRESHOLD = 15;
+// image 3D effect on hover scripting
+
+document.querySelectorAll('img').forEach(item => {
+	item.addEventListener('mousemove', handleHover, false)
+  })
+
+document.querySelectorAll('img').forEach(item => {
+	item.addEventListener('mouseleave', resetStyles, false)
+  })
+
+// hover angle threshold
+const THRESHOLD = 10;
+
 
 function handleHover(e) {
-	// TODO
+	const { clientX, clientY, currentTarget } = e;
+	const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget;
+
+	const horizontal = (clientX - offsetLeft) / clientWidth;
+	// added -window.scrollY to fix distortin on scroll
+	const vertical = (clientY - (offsetTop - window.scrollY)) / clientHeight;
+
+	const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2);
+	const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
+
+	this.style.transform =
+	`perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1.15, 1.15, 1.15)`;
 }
 
 function resetStyles(e) {
-	// TODO
+	this.style.transform =
+	`perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`;
 }
-
-card.addEventListener("mousemove", handleHover);
-card.addEventListener("mouseleave", resetStyles);
