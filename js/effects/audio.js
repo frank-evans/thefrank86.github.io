55a51a38e4532358c867c614c7efc1dbb6a45e84
Frank <thefrank86@gmail.com>
2023-08-29 23:47:43 -0500

    audio-context for safari test

--- js/effects/audio.js
+++ js/effects/audio.js
@@ -10,42 +10,58 @@ export function audioSunInit() {
 }
 	audioSun.volume = (0.05); */
 
-import { AudioContext } from 'https://jspm.dev/standardized-audio-context';
-let url  = "../../static/audio/sun5.ogg";
+import { AudioContext as audioCtx, decodeAudioData } from 'https://jspm.dev/standardized-audio-context';
+//let url  = "../../static/audio/sun5.ogg";
+//let url  = `<audio src="../../static/audio/sun5.ogg"></audio>`;
+let audioElement = document.querySelector("audio");
 export let audioSun;
 export let gainNode;
 
 export function audioSunInit() {
 	if (audioSun === undefined) {
-		audioSun = new AudioContext();
-		audioSun.resume();
+		audioSun = new audioCtx();
 		gainNode = audioSun.createGain();
 
-		let source = audioSun.createBufferSource();
-			//connect it to the destination so you can hear it.
-			source.connect(gainNode);
+		audioSun.createBuffer(2, 22050, 44100);
+		console.log(audioSun);
+		//let source = audioSun.createBufferSource();
+				//connect it to the destination so you can hear it.
+			//source.connect(gainNode);
+			let track = audioSun.createMediaElementSource(audioElement);
+			track.connect(gainNode);
 			gainNode.connect(audioSun.destination);
-			gainNode.gain.value = 0.05;
+			//gainNode.gain.value = 0.05;
+			gainNode.gain.value = 0.1;
 
-			/* --- load buffer ---  */
+			audioElement.loop = true;
+			audioElement.play();
+			audioSun.suspend();
+			/*async function safariAudio() {
+				const response = await fetch("../../static/audio/sun5.ogg");
+				const arrayBuffer = await response.arrayBuffer();
+				const audioBuffer = await decodeAudioData(audioSun, arrayBuffer);
+			}
+			safariAudio(); */
+				
+				/* --- load buffer ---  
 			let request = new XMLHttpRequest();
-			//open the request
+				//open the request
 			request.open('GET', url, true); 
-			//webaudio paramaters
+				//webaudio paramaters
 			request.responseType = 'arraybuffer';
-			//Once the request has completed... do this
+				//Once the request has completed... do this
 			request.onload = function() {
 				audioSun.decodeAudioData(request.response, function(response) {
-					/* --- play the sound AFTER the buffer loaded --- */
-					//set the buffer to the response we just received.
+						/* --- play the sound AFTER the buffer loaded --- 
+						//set the buffer to the response we just received.
 					source.buffer = response;
-					//start(0) should play asap.
+						//start(0) should play asap.
 					source.start(0);
 					source.loop = true;
 				}, function () { console.error('The request failed.'); } );
 			}
-			//Now that the request has been defined, actually make the request. (send it)
-			request.send();
+				//Now that the request has been defined, actually make the request. (send it)
+			request.send(); */
 	}
 }
 
